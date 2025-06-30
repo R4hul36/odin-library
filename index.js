b9a182ac92e3083fae16c548a79b8ce55c492cb0
Rahul <rahul.wa47@gmail.com>
2025-06-30 14:24:09 +0530

    added toggle functionality for read

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ function Book(title, author, pages, read, id) {
   // }
 }
 
+Book.prototype.toggleReadStatus = function () {
+  this.read = this.read === 'Yes' ? 'No' : 'Yes'
+}
+
 function addBookToLibrary(title, author, pages, read) {
   let uuid = crypto.randomUUID()
   const book = new Book(title, author, pages, read, uuid)
@@ -23,22 +27,20 @@ function addBookToLibrary(title, author, pages, read) {
 
 let tableBody = document.querySelector('.table-body')
 
-
-
 const addBookToDom = function () {
   tableBody.innerHTML = ''
   if (myLibrary.length == 0) {
-  const tableRow = document.createElement('tr')
-  const tableData = document.createElement('td')
-  tableData.textContent = 'There are no books to show yet...'
-  tableRow.appendChild(tableData)
-  tableData.colSpan = '6'
-  tableBody.appendChild(tableRow)
-  } 
-  
+    const tableRow = document.createElement('tr')
+    const tableData = document.createElement('td')
+    tableData.textContent = 'There are no books to show yet...'
+    tableRow.appendChild(tableData)
+    tableData.colSpan = '6'
+    tableBody.appendChild(tableRow)
+  }
 
   return myLibrary.forEach((book, index) => {
     const { author, id, pages, read, title } = book
+    console.log(read)
 
     const tableRow = document.createElement('tr')
 
@@ -53,6 +55,12 @@ const addBookToDom = function () {
 
     const readData = document.createElement('td')
     readData.textContent = read
+
+    let status = read === 'Yes' ? 'green' : 'red'
+
+    readData.classList.add(`toggle-read`)
+    readData.classList.add(`${status}`)
+    readData.setAttribute('data-book-id', id)
 
     const titleData = document.createElement('td')
     titleData.textContent = title
@@ -96,48 +104,75 @@ closeBtn.addEventListener('click', () => {
   dialog.close()
 })
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
+function handleSubmit(bookId) {
   const formData = new FormData(form)
   const title = formData.get('title')
   const author = formData.get('author')
   const pages = formData.get('pages')
   const read = formData.get('read')
+  console.log(read)
 
   let uuid = crypto.randomUUID()
-  // console.log(title, author, pages, read)
-  addBookToLibrary(title, author, pages, read, uuid)
+
+  if (bookId) {
+    const book = myLibrary.find((book) => book.id === bookId)
+    if (book) {
+      book.title = title
+      book.author = author
+      book.pages = pages
+      book.read = read
+    }
+  } else {
+    addBookToLibrary(title, author, pages, read, uuid)
+  }
+
   addBookToDom()
   dialog.close()
   form.reset()
-  
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  handleSubmit(form.getAttribute('data-edit-id'))
+  form.removeAttribute('data-edit-id')
 })
-
-
 
 tableBody.addEventListener('click', (e) => {
   const bookId = e.target.getAttribute('data-book-id')
   // Delete Book
   if (e.target.classList.contains('delete')) {
-    // console.log(e.target); 
-    myLibrary = myLibrary.filter(book => book.id !== bookId);
+    // console.log(e.target);
+    myLibrary = myLibrary.filter((book) => book.id !== bookId)
     addBookToDom()
-    
   }
 
   //edit book
   if (e.target.classList.contains('edit')) {
-    
-    console.log(e.target);
-    const {title, author, pages, read, id} = myLibrary.filter(book => book.id === bookId)[0]
-    document.querySelector('#title').value = title;
-    document.querySelector('#author').value = author;
-    document.querySelector('#pages').value = pages;
+    console.log(e.target)
+    const { title, author, pages, read, id } = myLibrary.filter(
+      (book) => book.id === bookId
+    )[0]
+    document.querySelector('#title').value = title
+    document.querySelector('#author').value = author
+    document.querySelector('#pages').value = pages
     document.querySelector('#read').value = read
+
+    form.setAttribute('data-edit-id', bookId)
     dialog.showModal()
   }
 
-});
+  if (e.target.classList.contains(`toggle-read`)) {
+    const book = myLibrary.find((book) => book.id === bookId)
+    if (book) {
+      book.toggleReadStatus()
+      addBookToDom()
+    }
+  }
+})
 
+// const readButton = document.querySelector('.toggle-read')
 
+// readButton.addEventListener('click', (e) => {
+//   console.log("ysdfsdf");
 
+// })
